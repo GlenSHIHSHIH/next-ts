8ddabe3a71d0d3f5518ab5d8aedfe76e5f71cf0d
GlenSHIHSHIH <a0935745150@gmail.com>
2021-10-31 00:38:26 +0800

    .env 調整 、 select 選單調整

---
+++ component/SelectBox.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useState } from 'react';
+
+interface SelectOption {
+    SelectName: string,
+    OptionValue: number[],
+    DefaultValue: number,
+}
+
+const SelectBox: React.FC<SelectOption> = (props) => {
+
+    const { OptionValue, SelectName } = props;
+    const [selectValue, setSelectValue] = useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setSelectValue(event.target.value as string);
+
+    };
+
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">{SelectName}</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={selectValue}
+                    label={SelectName}
+                    onChange={handleChange}
+                >
+                    {
+                        OptionValue.map((oValue) => {
+                            return (
+                                <MenuItem value={oValue}>{oValue}</MenuItem>
+                            )
+                        })
+                    }
+                </Select>
+            </FormControl>
+        </Box>
+    );
+}
+export default SelectBox;
