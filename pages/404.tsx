f998a4607072f28e830f5796f9209e636be0c5ea
Gary <Gary.shih@wvt.com.tw>
2021-12-08 14:01:24 +0800

    修改 404 頁面調整

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,3 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 // export const getStaticProps: GetStaticProps = async () => {
 //     return {
@@ -10,10 +8,38 @@ import { useEffect } from 'react';
 //     };
 // };
 
+import { Container, Grid } from "@mui/material";
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import styleProductionPage from "@styles/page/ProductionPage.module.css";
+import HeaderTitle from "component/HeaderTitle";
+import PageTitle from "component/PageTitle";
+import { useRouter } from "next/router";
+import React from "react";
+
+
 export default function Custom404() {
     const router = useRouter();
-    useEffect(() => {
-        router.push('/production/list');
-     }, [])
-    return (<h1>404 - Page Not Found</h1>)
+    // useEffect(() => {
+    //     router.push('/production/list');
+    //  }, [])
+
+    let theme = createTheme();
+    theme = responsiveFontSizes(theme);
+
+    return (
+        <Container maxWidth="xl">
+              <HeaderTitle
+                url={router.basePath + process.env.DEFAULT_HOME_URL}
+            />
+
+            <PageTitle
+                theme={theme}
+                className={styleProductionPage.pageTitle}
+                url={router.basePath + process.env.DEFAULT_HOME_URL}
+            />
+            <Grid container direction="column" justifyContent="center" alignItems="flex-start">
+                <h1>404 - Page Not Found</h1>
+            </Grid>
+        </Container>
+    )
 }
