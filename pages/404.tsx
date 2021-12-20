3ba98873ff90baf61d3ea370b0db1026c6ad759b
Gary <Gary.shih@wvt.com.tw>
2021-12-20 16:22:56 +0800

    修改 404 前後端 跳頁方式

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,45 +1,45 @@
-
-// export const getStaticProps: GetStaticProps = async () => {
-//     return {
-//         redirect: {
-//             destination: '/production/list',
-//             permanent: true,
-//         },
-//     };
-// };
-
 import { Container, Grid } from "@mui/material";
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import styleProductionPage from "@styles/page/ProductionPage.module.css";
 import HeaderTitle from "component/HeaderTitle";
 import PageTitle from "component/PageTitle";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 
 export default function Custom404() {
     const router = useRouter();
-    // useEffect(() => {
-    //     router.push('/production/list');
-    //  }, [])
-
     let theme = createTheme();
     theme = responsiveFontSizes(theme);
 
-    return (
-        <Container maxWidth="xl">
-              <HeaderTitle
-                url={router.basePath + process.env.DEFAULT_HOME_URL}
-            />
+    if(router.asPath.includes('/backstage',0)){
+        useEffect(() => {
+            router.push('/backstage/dashboard');
+         }, [])
+        return (
+            <Container maxWidth="xl">
+                <Grid container direction="column" justifyContent="center" alignItems="flex-start">
+                    <h1>404 - Page Not Found</h1>
+                </Grid>
+            </Container>
+        );
+    }else{
+         return (
+            <Container maxWidth="xl">
+                <HeaderTitle
+                    url={router.basePath + process.env.DEFAULT_HOME_URL}
+                />
 
-            <PageTitle
-                theme={theme}
-                className={styleProductionPage.pageTitle}
-                url={router.basePath + process.env.DEFAULT_HOME_URL}
-            />
-            <Grid container direction="column" justifyContent="center" alignItems="flex-start">
-                <h1>404 - Page Not Found</h1>
-            </Grid>
-        </Container>
-    )
+                <PageTitle
+                    theme={theme}
+                    className={styleProductionPage.pageTitle}
+                    url={router.basePath + process.env.DEFAULT_HOME_URL}
+                />
+                <Grid container direction="column" justifyContent="center" alignItems="flex-start">
+                    <h1>404 - Page Not Found</h1>
+                </Grid>
+            </Container>
+        )
+    }
+
 }
